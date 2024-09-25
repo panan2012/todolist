1369b79cbb66d13cfcd2d0db4850d4a9f0ca4e2b
panan2012 <nnuna2012@gmail.com>
2024-09-25 10:04:14 +0700

    first commit

---
+++ todoList/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref } from 'vue'
+const header = ref('Hello to Panan To Do List')
+const editing = ref(false)
+</script>
+
+<template >
+  <div class="header">
+    <h1>{{ header }}</h1>
+    <button v-if="editing" class="btn" @click="doEdit(false)">
+      Cancel
+    </button>
+    <button v-else class="btn btn-primary" @click="doEdit(true)">
+      Add Item
+    </button>
+  </div>
+  <form action="">
+  <input 
+      v-model.trim="newItem"
+      type="text" 
+      placeholder="Add an item"
+  >
+  <button class="btn btn-primary">
+  Save Item
+  </button>
+</form>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
